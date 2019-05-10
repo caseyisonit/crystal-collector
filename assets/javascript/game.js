9e9a5dfd6544630d8c77fd172e856091b61aa1f2
casey is on it <caseyeickhoffdesign@gmail.com>
2019-05-10 13:30:11 -0600

    figured out how to fit everything in a for loop. built out a start game function that also acts as a reset. GAME WORKS yeehaw 🤠

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,64 @@
-$(document).ready(function() {
-    
+
 // Global Variables wins, loses, target score, your score
 var wins = 0;
 var losses = 0;
-var target = generateTarget();
-var score = 0;
 
-$('#wins').text(wins);
-$('#losses').text(losses);
-$('#target').text(target);
-$('#score').text(score);
+function startGame() {
+    var target = randomNum(19, 120);
+    var score = 0;
+
+    $('#wins').text(wins);
+    $('#losses').text(losses);
+    $('#target').text(target);
+    $('#score').text(score);
+    $("#crystal").text("");
+
+    //dynamically create crystals on the page (one object per crystal)
+    //each crystal needs a score, an image, [aka a string holding the path to the image - which is saved in a folder in your project] an object with all of this info (aka collection of key value parts)
+
+    var crystalArray = ["assets/images/garnet.png", "assets/images/amethyst.png", "assets/images/pearl.png", "assets/images/steven.png"]
+
+    function createCrystals() {
+        for (var i = 0; i < crystalArray.length; i++) {
+            var crystal = $("<img>");
+            crystal.attr("src", crystalArray[i]);
+            crystal.addClass("gems");
+            crystal.attr("data-value", randomNum(1, 12));
+            $("#crystal").append(crystal);
+        }
+    }
+
+    createCrystals()
 
 
-//dynamically create crystals on the page (one object per crystal)
-//each crystal needs a score, an image, [aka a string holding the path to the image - which is saved in a folder in your project] an object with all of this info (aka collection of key value parts)
-var garnet = {score: crystalScore()}
-var amethyst = {score: crystalScore()}
-var pearl = {score: crystalScore()}
-var steven = {score: crystalScore()}
+    // Random number Function: 
+    function randomNum(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-$('#garnet').text(garnet);
-$('#amethyst').text(amethyst);
-$('#pearl').text(pearl);
-$('#steven').text(steven);
+    $('.gems').on("click", function () {
+        var gemScore = parseInt($(this).attr("data-value"));
+        var yourScore = score += gemScore;
+        $('#score').text(yourScore);
+        if (yourScore === target) {
+            wins++;
+            $('#wins').text(wins);
+            startGame();
+            console.log("You win! 🤩")
+        } else if (yourScore > target) {
+            losses++;
+            $('#losses').text(losses);
+            startGame();
+            console.log("YOU SUCK 🤬")
+        }
+    })
 
-console.log (garnet)
-console.log (amethyst)
-console.log (pearl)
-console.log (steven)
-
-// Random number Function: 
-function generateTarget(){
-    randomNum = Math.floor((Math.random() * 121) + 19)
-    return randomNum;
 }
 
-function crystalScore(){
-    randomNum = Math.floor((Math.random() * 12) + 1)
-    return randomNum;
-}
-
-
+$(document).ready(function () {
+    startGame();
 });
-
-
-//something with math...
-    //return a random number
-
-//Click event function
+    //Click event function
     //each crystal should have the same class so that you dont need 4 seperate click events
     //var score = yourScore + crystalValue;
 
